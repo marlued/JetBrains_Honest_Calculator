61cbdbf419304cee6b990fe1951ca619859044a6
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-06-24 13:04:02 +0200

    Working version according to the specifications for stage 2. All tests passed.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+
+def calculator(number_one, number_two, operator):
+    if operator == '+':
+        return float(number_one + number_two)
+    elif operator == '-':
+        return float(number_one - number_two)
+    elif operator == '*':
+        return float(number_one * number_two)
+    elif operator == '/' and number_two != 0:
+        return float(number_one / number_two)
+    elif operator == '/' and number_two == 0:
+        raise ZeroDivisionError
+
+
 message_1 = 'Enter an equation '
 message_2 = 'Do you even know what numbers are? Stay focused!'
 message_3 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+message_4 = 'Yeah... division by zero. Smart move...'
 OPERATORS = tuple('+ - * /'.split())
 
 while True:
 
     print(message_1)
 
-    x, operator, y = input().strip().split()
+    x, op, y = input().strip().split()
 
     try:
 
@@ -33,9 +48,18 @@ while True:
         print(message_2)
         continue
 
-    if operator not in OPERATORS:
-
+    if op not in OPERATORS:
         print(message_3)
         continue
 
+    try:
+
+        result = calculator(x, y, op)
+
+    except ZeroDivisionError:
+
+        print(message_4)
+        continue
+
+    print(result)
     break
